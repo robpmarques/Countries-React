5cdfeb744e9b7289654af7c167e0e1f82ac8d2ad
Roberto <44986570+matsufrg@users.noreply.github.com>
2020-01-20 19:57:23 -0300

    Mudanças para react hooks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
-import Main from './Pages/Main';
-import Detail from './Pages/Detail';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import App from './App';
 
-
-ReactDOM.render(
-<BrowserRouter>
-    <Switch>
-        <Route path="/" exact={true} component={Main} />
-        <Route path="/detail/:countryCode" component={Detail} />
-    </Switch>
-</BrowserRouter>,
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
